bbb7e4183fae444cc29ecc4c7d7cbe0bb494ca46
[vanshgusain] <vanshgusain18@gmail.com>
2024-06-27 14:02:42 +0530

    added database and connected index.js to it

---
+++ backend/db.js
@@ -0,0 +1,11 @@
+const mongoose = require("mongoose");
+const mongoURI = "mongodb://localhost:27017/";
+
+const connectToMongo = async () => {
+  await mongoose
+    .connect(mongoURI)
+    .then(() => console.log("Connected to Mongo Successfully"))
+    .catch((err) => console.log(err));
+};
+
+module.exports = connectToMongo;

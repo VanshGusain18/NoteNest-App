f9d72750533d8c896462a0ecbc0491d5d2774add
[vanshgusain] <vanshgusain18@gmail.com>
2024-06-29 18:36:57 +0530

    added a layer of validation through express-validator

--- backend/db.js
+++ backend/db.js
@@ -1,5 +1,5 @@
 const mongoose = require("mongoose");
-const mongoURI = "mongodb://localhost:27017/";
+const mongoURI = "mongodb://localhost:27017/NoteNest";
 
 const connectToMongo = async () => {
   await mongoose
--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,13 +1,35 @@
 const express = require("express");
 const User = require("../models/User");
+const { body, validationResult } = require("express-validator");
 const router = express.Router();
 
 // Create a User using POST "/api/auth". Doesn't require Auth
-router.get("/", (req, res) => {
-  console.log(req.body);
-  const user = User(req.body);
-  user.save();
-  res.send(req.body);
-});
+router.post(
+  "/",
+  [
+    body("name", "minimum length of name is 3").isLength({ min: 3 }),
+    body("email", "enter a valid email").isEmail(),
+    body("password", "minimum length of password is 5").isLength({ min: 5 }),
+  ],
+  (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+    User.create({
+      name: req.body.name,
+      email: req.body.email,
+      password: req.body.password,
+    })
+      .then((user) => res.json(user))
+      .catch((err) => {
+        console.log(err);
+        res.json({
+          error: "please enter a unique email",
+          message: err.message,
+        });
+      });
+  }
+);
 
 module.exports = router;

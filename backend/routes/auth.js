8a3395cc3859960f5ae434ffde02083fca6d814c
[vanshgusain] <vanshgusain18@gmail.com>
2024-07-01 19:10:09 +0530

    formated auth file for better understanding

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -5,30 +5,31 @@ const router = express.Router();
 
 // Create a User using POST "/api/auth/createuser". No login required
 router.post(
-  "createuser/",
+  "/createuser",
   [
     body("name", "minimum length of name is 3").isLength({ min: 3 }),
     body("email", "enter a valid email").isEmail(),
     body("password", "minimum length of password is 5").isLength({ min: 5 }),
   ],
-  (req, res) => {
+  async (req, res) => {
+    // If there are errors, return errors and bad request.
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
       return res.status(400).json({ errors: errors.array() });
     }
-    User.create({
+    // Check user with the same email exists already.
+    let user = await User.findOne({ email: req.body.email });
+    if (user) {
+      return res
+        .status(400)
+        .json({ error: "Sorry a user with this email already exists" });
+    }
+    user = await User.create({
       name: req.body.name,
       email: req.body.email,
       password: req.body.password,
-    })
-      .then((user) => res.json(user))
-      .catch((err) => {
-        console.log(err);
-        res.json({
-          error: "please enter a unique email",
-          message: err.message,
-        });
-      });
+    });
+    res.json({ user });
   }
 );
 

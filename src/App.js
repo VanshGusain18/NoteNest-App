aeafb1ed1309a3f7e2b012a122d3d4daabcaf181
[vanshgusain] <vanshgusain18@gmail.com>
2024-07-10 20:34:54 +0530

    added routes for navbar

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
 import "./App.css";
+import About from "./Components/About";
+import Home from "./Components/Home";
 import Navbar from "./Components/Navbar";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
     <>
-      <Navbar />
-      <h1>this is notenest</h1>
+      <Router>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route exact path="/about" element={<About />} />
+        </Routes>
+      </Router>
     </>
   );
 }

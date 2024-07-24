671841b24fa450667727e3a776ec3342a07a05c8
[vanshgusain] <vanshgusain18@gmail.com>
2024-07-24 10:47:02 +0530

    added validation layer on updatenote too(backend)

--- backend/routes/notes.js
+++ backend/routes/notes.js
@@ -49,41 +49,55 @@ router.get("/getnotes", fetchuser, async (req, res) => {
 });
 
 //ROUTE3: update notes using PUT "/api/notes/updatenotes". login required
-router.put("/updatenotes/:id", fetchuser, async (req, res) => {
-  try {
-    const { title, description, tag } = req.body;
-    //create a newNote obj.
-    const newNote = {};
-    if (title) {
-      newNote.title = title;
-    }
-    if (description) {
-      newNote.description = description;
-    }
-    if (tag) {
-      newNote.tag = tag;
-    }
+router.put(
+  "/updatenotes/:id",
+  fetchuser,
+  [
+    body("title", "minimum length of title is 3").isLength({ min: 3 }),
+    body("description", "minimum length of description is 5").isLength({
+      min: 5,
+    }),
+  ],
+  async (req, res) => {
+    try {
+      const { title, description, tag } = req.body;
+      const errors = validationResult(req);
+      if (!errors.isEmpty()) {
+        return res.status(400).json({ errors: errors.array() });
+      }
+      //create a newNote obj.
+      const newNote = {};
+      if (title) {
+        newNote.title = title;
+      }
+      if (description) {
+        newNote.description = description;
+      }
+      if (tag) {
+        newNote.tag = tag;
+      }
 
-    //find the note to be updated and update it.
-    let note = await Note.findById(req.params.id);
-    if (!note) {
-      return res.status(400).send("Not Found");
-    }
-    if (note.user.toString() != req.user.id) {
-      return res.status(401).send("Not Allowed");
-    }
+      //find the note to be updated and update it.
+      let note = await Note.findById(req.params.id);
+      if (!note) {
+        return res.status(400).send("Not Found");
+      }
+      if (note.user.toString() != req.user.id) {
+        return res.status(401).send("Not Allowed");
+      }
 
-    note = await Note.findByIdAndUpdate(
-      req.params.id,
-      { $set: newNote },
-      { new: true }
-    );
-    res.json(note);
-  } catch (error) {
-    console.error(error.message);
-    res.status(500).send("Internal Server Error");
+      note = await Note.findByIdAndUpdate(
+        req.params.id,
+        { $set: newNote },
+        { new: true }
+      );
+      res.json(note);
+    } catch (error) {
+      console.error(error.message);
+      res.status(500).send("Internal Server Error");
+    }
   }
-});
+);
 
 //ROUTE4: delete notes using PUT "/api/notes/deletenotes". login required
 router.delete("/deletenotes/:id", fetchuser, async (req, res) => {

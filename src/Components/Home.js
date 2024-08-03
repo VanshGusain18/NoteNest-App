5ebbf2c7ff9dfba92c0377bc64648ac21f31300f
[vanshgusain] <vanshgusain18@gmail.com>
2024-08-03 08:30:46 +0530

    formating the home page a bit

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,9 +1,19 @@
-import React from "react";
+import React, { useContext } from "react";
 import Note from "./Note";
+import AuthContext from "../Context/notes/AuthContext";
 const Home = () => {
+  const contex = useContext(AuthContext);
+  const { authtoken } = contex;
+
   return (
     <div className="container my-2">
-      <Note />
+      {authtoken === null ? (
+        <p class="d-flex justify-content-center">
+          Login or Signup to use NoteNest
+        </p>
+      ) : (
+        <Note />
+      )}
     </div>
   );
 };
